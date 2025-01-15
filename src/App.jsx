634d18ef8ca72613125d93d39b66158fe236d194
Abhinavreddy-123 <abhinavpolala@gmail.com>
2025-01-15 19:46:27 +0530

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 function App() {
-  return;
-  <></>;
+  return (
+    <>
+      <main>
+        <section>
+          {/* here the logo and nav bar comes */}
+          <header>
+            <img src="" alt="" />
+            <nav></nav>
+          </header>
+        </section>
+
+        <section>{/* hero section goes here */}</section>
+      </main>
+    </>
+  );
 }
 
 export default App;
